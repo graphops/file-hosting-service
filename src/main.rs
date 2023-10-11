2439e0164128e8592bcc028c4105a1f0c7d7a2d8
hopeyen <hopey1118@gmail.com>
2023-10-10 20:54:45 -0500

    feat: add ipfs query retries

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use clap::Parser;
+
 use config::{Cli, Role};
 use ipfs::IpfsClient;
 // use ipfs_api_prelude::*;
 use std::time::Duration;
+use tokio_retry::strategy::{jitter, ExponentialBackoff};
+use tokio_retry::Retry;
 // use ipfs_api_backend_hyper::{IpfsClient, IpfsApi};
 use dotenv::dotenv;
 use std::fs;
@@ -66,8 +68,12 @@ async fn fetch_subfile_from_ipfs(
     // Fetch the content from IPFS
     let timeout = Duration::from_secs(30);
 
-    // let file_bytes = client.cat(ipfs_hash, Some(timeout)).await?;
-    let file_bytes = client.cat_all(ipfs_hash, timeout).await?;
+    // let file_bytes = client.cat_all(ipfs_hash, timeout).await?;
+    let retry_strategy = ExponentialBackoff::from_millis(10)
+        .map(jitter) // add jitter to delays
+        .take(5); // limit to 5 retries
+
+    let file_bytes = Retry::spawn(retry_strategy, || client.cat_all(ipfs_hash, timeout)).await?;
 
     // let data = String::from_utf8(file_bytes.to_vec()).unwrap();
 
@@ -78,28 +84,30 @@ async fn fetch_subfile_from_ipfs(
     let content: serde_yaml::Value =
         serde_yaml::from_str(&String::from_utf8(file_bytes.to_vec())?)?;
 
-    tracing::info!(
-        content = tracing::field::debug(&content),
-        "Read file content"
-    );
+    tracing::info!("Got yaml file content");
 
     let subfile = convert_to_subfile(content)?;
-
-    // d["foo"]["bar"]
-    //     .as_str()
-    //     .map(|s| s.to_string())
-    //     .ok_or(anyhow!("Could not find key foo.bar in something.yaml")
 
     Ok(subfile)
 }
 
 fn convert_to_subfile(value: serde_yaml::Value) -> Result<Subfile, anyhow::Error> {
+    tracing::trace!(
+        value = tracing::field::debug(&value),
+        "Parse yaml value into a subfile"
+    );
     let subfile: Subfile = serde_yaml::from_value(value)?;
     Ok(subfile)
 }
 
 async fn leech(client: &IpfsClient, ipfs_hash: &str) -> Result<Subfile, anyhow::Error> {
     // Grab subfile.yaml from IPFS
+
+    // // let file_bytes = client.cat(ipfs_hash, Some(timeout)).await?;
+    // let retry_strategy = ExponentialBackoff::from_millis(10)
+    // .map(jitter) // add jitter to delays
+    // .take(5);    // limit to 5 retries
+
     let subfile: Subfile = fetch_subfile_from_ipfs(client, ipfs_hash).await?;
 
     // Request torrent tracker and download
