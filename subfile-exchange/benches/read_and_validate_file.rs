01d5a06c3a8cd81abf57efacf797ab81b6b31079
hopeyen <hopey1118@gmail.com>
2023-11-29 08:50:08 -0600

    perf: benchmark read_and_validate_file fn

---
+++ subfile-exchange/benches/read_and_validate_file.rs
@@ -0,0 +1,78 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use subfile_exchange::{
+    file_hasher::ChunkFile,
+    file_reader::read_and_validate_file,
+    publisher::{FileMetaInfo, SubfileManifest},
+    types::{BlockRange, Subfile},
+};
+
+fn setup() -> (Subfile, ChunkFile) {
+    let chunk_file = ChunkFile {
+        file_name: "0017234600.dbin.zst".to_string(),
+        total_bytes: 26359000,
+        chunk_size: 1048576,
+        chunk_hashes: [
+            "MIHJRsl2+fhrbTX9XQxR5/THummB4NfjSdlpUiwc4KM=".to_string(),
+            "ETtu0ZbJOg/HCfpcWVafBLTo52tTplf7XaI7FC28f3g=".to_string(),
+            "egmYeJKDDLUb4w5V2MWtWa+7OYU2qkKEjNtpi4beYFQ=".to_string(),
+            "IXF0ko986rGGShWasNbvUfi42z9HzILfum+5saZORNE=".to_string(),
+            "EWsdOfZ4Kxyt2gQz2atAqxVyW+uNQrVRDV5B/YDWNqU=".to_string(),
+            "SDbCTKYd0yXZjFHYx5jBL1f4sXOoMbe+XHbBGB4lr/c=".to_string(),
+            "bFmY9ZX9kNDX2IOiNFpQ3PXpANgWB7sT13VzQzFoCW4=".to_string(),
+            "0gJv2+ugSEu5YObHCGMlPachg4a1rBLhjRC6h9vypUo=".to_string(),
+            "Mb0sJCNBlDsX79lgS5upYMxE/Ogur3k5w8zV56xsujc=".to_string(),
+            "mGi8bm5zqXyqNffmMKZxeMrOhCRD/a/qEhsyfyEoSWo=".to_string(),
+            "54oAlwwO5AtDC66rsr54jQg4nzeBhILx0Yu3WRkposs=".to_string(),
+            "mJNLGP5xg9crtEEodz86jEdH0/tI/8EN+O/EuIdMkso=".to_string(),
+            "8yL8ga/aLbe+h/SNvdbX3RY1fN4U1P7nAHhoWTuuZKY=".to_string(),
+            "Fg4OPNn5UH86DKpxKW0UsSj8qZ89Uw79zb+/xf1kajk=".to_string(),
+            "rT3gyX+48dbOUuthys0xAniCDmAtal7Zur/ajztyVOk=".to_string(),
+            "+gW9KCoZfEobqis9S6p4aNZoCeKLldUz/CzKn4k6hxs=".to_string(),
+            "y3Z+xH33uexkeGZxVe1eI0oYzUrrkeEJcgdzYjcwt3w=".to_string(),
+            "SAPD+TVgBeE9eAWe8kbzTGQYlDdfcmz9kmatByu4Bos=".to_string(),
+            "+X30R3P65u5ealfn+zoypJEfodCHxxC98g3RMKgOmVg=".to_string(),
+            "fFt5P452EBAPWeRCwLy+Na0BHRU0dLANmsceSroMQ2U=".to_string(),
+            "skkMxFfH6yhrcaePxsOR6ux8LpFhcJ3V+dMXr9NeyR8=".to_string(),
+            "p4G97Y6W/FQ8nR0lfz4YwKyPDQEsU8Ix5Rk6MM9h1x4=".to_string(),
+            "OYMcj2nPkTghVJRFcgQt7MvurRC5a8tSfe4bst67LfI=".to_string(),
+            "1nMl3Q0EdbpXxlZFsiKYOz/qr+MfcIosJMsEOJgCWJ4=".to_string(),
+            "K2emdcJC2feqUjlK8C599XQqyAqbaY9dHwgzn+MmFp0=".to_string(),
+            "LjPfv0JdXsPixi7LxdrcjVAVknRCUq9yDUVpGKOF3Sw=".to_string(),
+        ]
+        .to_vec(),
+    };
+    let subfile = Subfile {
+        ipfs_hash: "QmUqx9seQqAuCRi3uEPfa1rcS61rKhM7JxtraL81jvY6dZ".to_string(),
+        local_path: "./example-file/".into(),
+        manifest: SubfileManifest {
+            files: [FileMetaInfo {
+                name: "0017234600.dbin.zst".to_string(),
+                hash: "QmadNB1AQnap3czUime3gEETBNUj7HHzww6hVh5F6w7Boo".to_string(),
+            }]
+            .to_vec(),
+            file_type: "flatfiles".to_string(),
+            spec_version: "0.0.0".to_string(),
+            description: "random flatfiles".to_string(),
+            chain_id: "0".to_string(),
+            block_range: BlockRange {
+                start_block: None,
+                end_block: None,
+            },
+        },
+        chunk_files: [chunk_file.clone()].to_vec(),
+    };
+
+    (subfile, chunk_file)
+}
+
+fn read_and_validate_file_benchmark(c: &mut Criterion) {
+    let (subfile, chunk_file) = black_box(setup());
+
+    c.bench_function("read_and_validate_file", |b| {
+        b.iter(|| read_and_validate_file(&subfile, &chunk_file))
+    });
+}
+
+criterion_group!(benches, read_and_validate_file_benchmark);
+criterion_main!(benches);
